3e3496c2ed84891bf0888d654087195f4b436e34
todoom1 <aa13976383@outlook.com>
2025-12-25 01:30:20 +0800

    Update list.js

--- list.js
+++ list.js
@@ -16,8 +16,8 @@ document.getElementById("zt").addEventListener("click", function() {
 document.getElementById("ff").addEventListener("click", function() {
   window.location.href = "https://bakmry.gziasti.com/app/register.php?site_id=1050&topId=277722&selfPlanId=174525";
 });
-document.getElementById('st').addEventListener('click', function() {
-	alert("胜天国际将预定于12月底上线！");
+document.getElementById("st").addEventListener("click", function() {
+  window.location.href = "https://tfajn.hcjwj.com/app/register.php?site_id=1058&topId=26489&selfPlanId=21600";
 });
 
 document.getElementById("0").innerHTML="🔥十年信誉平台推荐";
@@ -125,5 +125,6 @@ document.getElementById("54").innerHTML="虚拟币";
 document.getElementById("55").innerHTML="其他分享&VPN-开VPN可进蓝名";
 
 document.getElementById("56").innerHTML="如遇本站功能无法正常使用，请点击下载注册狗狗云VPN，即可突破网络封锁正常使用";
+
 
 document.getElementById("57").innerHTML="TC22.MY";
